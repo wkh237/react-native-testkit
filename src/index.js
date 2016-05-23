468b3c4a8b5375517df2c3dd60613e691b400669
Ben Hsieh <xeiyan@gmail.com>
2016-05-23 12:56:18 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import TestContext from './test-context'
+import Reporter from './components/reporter'
+import Assert from './components/assert'
+import Info from './components/info'
+
+export default {
+  TestContext,
+  Reporter,
+  Info,
+  Assert
+}
